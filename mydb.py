98860803b0b363a174b3f5639c9fa9926bdf8acf
Archit gupta <archit241001@gmail.com>
2023-04-01 15:15:06 +0530

    initial commit

---
+++ mydb.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'archit2001@1'
+)
+
+#prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# create database
+cursorObject.execute("CREATE DATABASE elderco")
+
+print("all done")
